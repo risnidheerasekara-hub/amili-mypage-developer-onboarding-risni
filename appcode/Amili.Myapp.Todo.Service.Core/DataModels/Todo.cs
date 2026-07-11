401619b9116aac84c297d876ddabc62f67bd074a
Risni Dheerasekara <risni.dheerasekara@visma.com>
2026-07-11 08:03:58 +0530

    add [Table] annotation

--- appcode/Amili.Myapp.Todo.Service.Core/DataModels/Todo.cs
+++ appcode/Amili.Myapp.Todo.Service.Core/DataModels/Todo.cs
@@ -1,22 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Amili.Myapp.Todo.Service.Core.DataModels;
 
+[Table("Todo")]
 public class Todo
 {
+    [Key]
+    [Column(TypeName = "bigint")]
     public long Id { get; set; }
 
+    [Column(TypeName = "varchar(255)")]
+    [StringLength(255)]
     public string Name { get; set; } = string.Empty;
 
+    [Column(TypeName = "text")]
     public string? Description { get; set; }
 
+    [Column(TypeName = "timestamptz")]
     public DateTime CreatedAt { get; set; }
 
     public bool IsCompleted { get; set; }
 
+    [Column(TypeName = "timestamptz")]
     public DateTime? CompletedAt { get; set; }
 }
