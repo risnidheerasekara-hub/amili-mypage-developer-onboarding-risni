869a1ddd59afe3a39d671584612326aff5f7bff0
Risni Dheerasekara <risni.dheerasekara@visma.com>
2026-07-11 12:35:41 +0530

    Remove unnecessary using directives from CreateTodoRequest and TodoResponse models

--- appcode/Amili.Myapp.Todo.Service.Core/Models/Response/TodoResponse.cs
+++ appcode/Amili.Myapp.Todo.Service.Core/Models/Response/TodoResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Amili.Myapp.Todo.Service.Core.Models.Response;
+﻿namespace Amili.Myapp.Todo.Service.Core.Models.Response;
 
 public class TodoResponse
 {
