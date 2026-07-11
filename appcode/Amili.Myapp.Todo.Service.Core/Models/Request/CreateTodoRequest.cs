760c9c2795d101ab0be6bd7a8d46ef8b0b6f245b
Risni Dheerasekara <risni.dheerasekara@visma.com>
2026-07-11 08:03:38 +0530

    rename request files

--- appcode/Amili.Myapp.Todo.Service.Core/Models/Request/CreateTodoRequest.cs
+++ appcode/Amili.Myapp.Todo.Service.Core/Models/Request/CreateTodoRequest.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Amili.Myapp.Todo.Service.Core.Models.Request;
 
-public class UpdateTodo
+public class CreateTodoRequest
 {
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public bool? IsCompleted { get; set; }
-}
+    [Required]
+    [StringLength(100)]
+    public required string Name { get; set; } = default!;
 
+    [StringLength(256)]
+    public string? Description { get; set; }
+}
--- appcode/Amili.Myapp.Todo.Service.Core/Models/Request/UpdateTodoRequest.cs
+++ appcode/Amili.Myapp.Todo.Service.Core/Models/Request/UpdateTodoRequest.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Amili.Myapp.Todo.Service.Core.Models.Request;
 
-public class CreateTodo
+public class UpdateTodoRequest
 {
-    [Required]
-    [StringLength(100)]
-    public string Name { get; set; } = string.Empty;
-
-    [StringLength(256)]
+    public string? Name { get; set; }
     public string? Description { get; set; }
 }
+
