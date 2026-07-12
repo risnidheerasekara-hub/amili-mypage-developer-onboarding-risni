f26ac4b79e74dae0fd6d07f4ec1c0f2c09928aa0
Risni Dheerasekara <risni.dheerasekara@visma.com>
2026-07-12 09:20:04 +0530

    Add unit test for retrieving todo item by id in TodoServiceUnitTests

--- appcode/Amili.Myapp.Todo.Service.Test/Implementation/TodoServiceUnitTests.cs
+++ appcode/Amili.Myapp.Todo.Service.Test/Implementation/TodoServiceUnitTests.cs
@@ -1,11 +1,11 @@
 using Amili.Myapp.Todo.Service.Core.Models.Request;
-using Amili.Myapp.Todo.Service.Implementation.Data;
 using Amili.Myapp.Todo.Service.Implementation.Mapper;
 using Amili.Myapp.Todo.Service.Implementation.Services;
 using Amili.Myapp.Todo.Service.Test.InMemoryDb;
 using AutoMapper;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
+using Datamodels = Amili.Myapp.Todo.Service.Core.DataModels;
 
 namespace Amili.Myapp.Todo.Service.Test.Implementation;
 
@@ -26,7 +26,7 @@ public class TodoServiceUnitTests
         _todoService = new TodoService(_dbContext, mapper);
     }
 
-    [Fact(DisplayName = "Create Todo | Success")]
+    [Fact(DisplayName = "Create Todo item")]
     public async Task Should_ReturnCreatedAtActionResult_When_CreateTodoItem_WithValidRequest()
     {
         var request = new CreateTodoRequest { Name = "Buy groceries", Description = "Milk, eggs, bread" };
@@ -38,4 +38,33 @@ public class TodoServiceUnitTests
         Assert.Equal(request.Description, result.Description);
         Assert.Single(_dbContext.Todos);
     }
+
+    [Fact(DisplayName = "Get todo item with existing id")]
+    public async Task Should_ReturnTodo_When_TodoExists()
+    {
+        var todo = new Datamodels.Todo
+        {
+            Name = "Test Todo",
+            Description = "Test Description",
+            CreatedAt = DateTime.UtcNow,
+            IsCompleted = false,
+            CompletedAt = null
+        };
+
+        _dbContext.Add(todo);
+        await _dbContext.SaveChangesAsync();
+
+        var result = await _todoService.GetTodoByIdAsync(todo.Id);
+
+        Assert.NotNull(result);
+        Assert.Equal(todo.Id, result.Id);
+        Assert.Equal(todo.Name, result.Name);
+        Assert.Equal(todo.Description, result.Description);
+        Assert.Equal(todo.CreatedAt, result.CreatedAt);
+        Assert.Equal(todo.IsCompleted, result.IsCompleted);
+        Assert.Equal(todo.CompletedAt, result.CompletedAt);
+    }
+
+
+
 }
