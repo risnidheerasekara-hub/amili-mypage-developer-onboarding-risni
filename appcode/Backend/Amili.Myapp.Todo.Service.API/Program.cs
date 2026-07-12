9db5de3d7a7e96402dfebfd221cc2780f304cacc
Risni Dheerasekara <risni.dheerasekara@visma.com>
2026-07-12 20:49:47 +0530

    move backend to new path  API with CRUD operations

--- appcode/Backend/Amili.Myapp.Todo.Service.API/Program.cs
+++ appcode/Backend/Amili.Myapp.Todo.Service.API/Program.cs
@@ -17,11 +17,20 @@ builder.Services.AddDbContext<TodoDbContext>(options =>
     );
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactDev", policy =>
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+app.UseCors("AllowReactDev");
 
 app.UseAuthorization();
 
